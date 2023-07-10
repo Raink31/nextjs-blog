39fccce96a31f35adda52a941ed0a103f0837637
Yohan Baes <yohan.baes97@gmail.com>
2023-07-10 10:46:22 +0200

    changed index

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hello, I’m Yohan. I’m a frontend developer, currently learning next. You can contact me on <a href="https://twitter.com/Yobai_Dev" target='_blank'>Twitter</a>.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
